3ff9999ffd3be10f3332f95b509e022cb9b1bd30
Piotr Przybylak <piotr.przybylak@gmail.com>
2022-01-24 20:07:06 +0100

    Add v1 of links page

--- src/main/java/io/bliki/demo/DemoController.java
+++ src/main/java/io/bliki/demo/DemoController.java
@@ -1,6 +1,7 @@
 package io.bliki.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,17 @@ public class DemoController {
     @GetMapping("/")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/v1")
+    public String v1(Model model) {
+        model.addAttribute("links", new String[]{
+                "https://kodologia.pl/blog/czym-jest-system-kontroli-wersji",
+                "https://docs.github.com/en/get-started/signing-up-for-github/signing-up-for-a-new-github-account",
+                "https://www.wikihow.com/Create-an-Account-on-GitHub",
+                "https://www.figma.com/",
+                "https://balsamiq.com/",
+        });
+        return "v1";
     }
 }
