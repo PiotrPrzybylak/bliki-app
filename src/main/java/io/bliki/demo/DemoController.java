1e5bf7df48d5fa355b14872d6f7651ccf0f70214
Piotr Przybylak <piotr.przybylak@gmail.com>
2022-01-24 20:48:58 +0100

    Refactor

--- src/main/java/io/bliki/demo/DemoController.java
+++ src/main/java/io/bliki/demo/DemoController.java
@@ -14,13 +14,7 @@ public class DemoController {
 
     @GetMapping("/v1")
     public String v1(Model model) {
-        model.addAttribute("links", new String[]{
-                "https://kodologia.pl/blog/czym-jest-system-kontroli-wersji",
-                "https://docs.github.com/en/get-started/signing-up-for-github/signing-up-for-a-new-github-account",
-                "https://www.wikihow.com/Create-an-Account-on-GitHub",
-                "https://www.figma.com/",
-                "https://balsamiq.com/",
-        });
+        model.addAttribute("links", getLinks());
         return "v1";
     }
 
