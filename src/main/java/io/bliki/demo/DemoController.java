59ee2e97d760bd859dea214863c5e2e1a86679ec
Piotr Przybylak <piotr.przybylak@gmail.com>
2022-01-24 20:44:05 +0100

    Add v2 of links page

--- src/main/java/io/bliki/demo/DemoController.java
+++ src/main/java/io/bliki/demo/DemoController.java
@@ -23,4 +23,16 @@ public class DemoController {
         });
         return "v1";
     }
+
+    @GetMapping("/v2")
+    public String v2(Model model) {
+        model.addAttribute("links", new Link[]{
+                new Link("https://kodologia.pl/blog/czym-jest-system-kontroli-wersji","Założenie konta na GitHubie"),
+                new Link("https://docs.github.com/en/get-started/signing-up-for-github/signing-up-for-a-new-github-account", "Założenie konta na GitHubie"),
+                new Link( "https://www.wikihow.com/Create-an-Account-on-GitHub", "Założenie konta na GitHubie"),
+                new Link("https://www.figma.com/", "Figma"),
+                new Link( "https://balsamiq.com/", "Balsamiq Wireframes"),
+        });
+        return "v2";
+    }
 }
---
+++ src/main/java/io/bliki/demo/Link.java
@@ -0,0 +1,4 @@
+package io.bliki.demo;
+
+public record Link(String href, String text) {
+}
