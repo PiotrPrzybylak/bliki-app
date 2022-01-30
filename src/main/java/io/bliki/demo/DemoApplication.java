1f3d145b3b3202715e7185a89370061b751ffaff
Piotr Przybylak <piotr.przybylak@gmail.com>
2022-01-30 23:33:26 +0100

    Add security

--- src/main/java/io/bliki/demo/DemoApplication.java
+++ src/main/java/io/bliki/demo/DemoApplication.java
@@ -2,12 +2,29 @@ package io.bliki.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+
+    @Configuration
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .antMatchers("/admin/*").authenticated()
+                    .anyRequest().permitAll();
+        }
+    }
 }
