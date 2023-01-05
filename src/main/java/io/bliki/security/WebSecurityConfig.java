74187be2158c9b842550fec00bb1e9a8a9e0454e
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-01-05 16:43:30 +0100

    Upgrage Spring Boot to 3.0.1

--- src/main/java/io/bliki/security/WebSecurityConfig.java
+++ src/main/java/io/bliki/security/WebSecurityConfig.java
@@ -4,25 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/admin/*").authenticated()
-                .anyRequest().permitAll()
-                .and()
+                .authorizeHttpRequests((authz) -> authz
+                                .requestMatchers("/admin/*")
+                                .authenticated()
+                                .anyRequest().permitAll()
+                )
                 .formLogin()
                 .permitAll()
                 .and()
                 .logout();
+        return http.build();
     }
 
     @Bean
